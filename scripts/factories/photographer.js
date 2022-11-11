08de26fbe88a56f1fc29c699d58d5651d3f966fb
AlexFreyjr <alexfreyjr@gmail.com>
2022-11-11 18:50:40 +0100

    homepage HTML/CSS done, need to debug JS return data

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,49 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
+    //catch data
+    const { name, portrait, city, country, tagline, price} = data;
+    //structure data for use in html
     const picture = `assets/photographers/${portrait}`;
+    const location = `${city}, ${country}`;
+    const blorb = tagline;
+    const fees = `${price}€/jour`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        //image
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt"," ");
+        //link
+        const a = document.createElement('a');
+        a.setAttribute("href","./photographer.html");
+        //title
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        //description
+        const divDescription = document.createElement('div');
+        divDescription.setAttribute("class","description");
+        //city
+        const divLocation = document.createElement('div');
+        divLocation.setAttribute("class","city");
+        divLocation.textContent = location;       
+        //blorb
+        const divBlorb = document.createElement('div');
+        divBlorb.setAttribute("class","blorb");
+        divBlorb.textContent = blorb; 
+        //fees
+        const divFees = document.createElement('div');
+        divFees.setAttribute("class","fees");
+        divFees.textContent = fees; 
+        
+        //article structure
+        article.appendChild(a);
+        a.appendChild(img);
+        a.appendChild(h2);
+        a.appendChild(divDescription);
+        divDescription.appendChild(divLocation);
+        divDescription.appendChild(divBlorb);
+        divDescription.appendChild(divFees);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, location, blorb, fees, getUserCardDOM }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,26 @@
     async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        
+        fetch('./data/photographers.json')
+            .then(response => {
+                //get the answers from the server to know if the fetch has happened correctly
+                //if not then display the error code
+                if (!response.ok){
+                    throw new Error("HTTP error" + response.status);
+                }
+                //return the json fetch by the server
+                return response.json();
+            })
+            //grab the json and modify it 
+            .then(json => {
+                //get only the photographer part of the json
+                let dataPhotographers = json.photographers;
+                //return only destructered data to be use in the init() function
+                return ({photographers: [...dataPhotographers]})
+            })
+            //catch error if something goes wrong when modifying the json
+            .catch(function(){
+                this.dataError = true;
+            })
     }
 
     async function displayData(photographers) {
@@ -37,6 +35,7 @@
 
     async function init() {
         // Récupère les datas des photographes
+        console.log(getPhotographers());
         const { photographers } = await getPhotographers();
         displayData(photographers);
     };
