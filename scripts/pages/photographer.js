27e05e2aff89c82829780e0b59d6acb3058af183
AlexFreyjr <alexfreyjr@gmail.com>
2022-11-15 17:17:54 +0100

    update photographer html

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,10 @@
 //Mettre le code JavaScript lié à la page photographer.html
+function displayLightbox() {
+    const lightbox = document.querySelector("#lightbox");
+	lightbox.style.display = "block";
+}
+
+function closeLightbox() {
+    const lightbox = document.querySelector("#lightbox");
+    lightbox.style.display = "none";
+}
