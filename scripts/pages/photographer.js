aa912e3c2e134c2957af0ba81c17678515374ad3
AlexFreyjr <alexfreyjr@gmail.com>
2022-11-16 16:24:46 +0100

    add total like functions to photographer

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,4 +1,5 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
 function displayLightbox() {
     const lightbox = document.querySelector("#lightbox");
 	lightbox.style.display = "block";
@@ -8,3 +9,22 @@ function closeLightbox() {
     const lightbox = document.querySelector("#lightbox");
     lightbox.style.display = "none";
 }
+function likeListener(){
+    let allHeart = document.querySelectorAll(".heart");
+    let likeNbr = document.querySelectorAll(".likeNbr");
+    allHeart.forEach((e) => e.addEventListener("click", function (e){
+        //select the number near
+        //add 1
+        //return the updated number
+      }));
+}
+
+function likesCount(){
+    let likeNbr = document.querySelectorAll(".likeNbr");
+    let result = 0;
+    likeNbr.forEach(e => {result = result + parseInt(e.innerHTML);}) 
+    return(result); 
+}
+
+document.querySelector(".totalLikes").innerHTML = likesCount();
+likeListener();
